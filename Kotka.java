26706002613a8c52e0913a9380c2e63b5b631a4b
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-05-29 10:31:47 +0300

    initial commit

---
+++ Kotka.java
@@ -0,0 +1,54 @@
+public class Kotka {
+	// полета
+	private String ime;
+	private String cvqt;
+	private int vazrast;
+	private double teglo;
+	private int position;
+	
+	// конструктори
+	public Kotka() {}
+	
+	public Kotka(int v) {
+		vazrast = v;
+	}
+	
+	public Kotka(String i, String c, int v, double t) {
+		ime = i;
+		cvqt = c;
+		vazrast = v;
+		teglo = t;
+	}
+	
+	// getter-и
+	public String getIme() {
+		return ime;
+	}
+	
+	public int getVazrast() {
+		return vazrast;
+	}
+	
+	// setter-и
+	public void setVazrast(int v) {
+		if (v >= 0 && v <= 100) {
+			vazrast = v;
+		}
+	}
+	
+	// други методи
+	public void print() {
+		System.out.println("Аз съм " + ime + ".");
+		System.out.println("Цветът ми е " + cvqt + ".");
+		System.out.println("На " + vazrast + " години съм.");
+		System.out.println("Тежа " + teglo + " килограма.");
+	}
+	
+	public void moveRight() {
+		position++;
+	}
+	
+	public void moveLeft() {
+		position--;
+	}
+}
